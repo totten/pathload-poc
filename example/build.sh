9c01b672b867233d8a956341c02d6b3920a285f5
Tim Otten <totten@civicrm.org>
2024-02-12 01:27:11 -0800

    example - Build some *.php variants of each library

--- example/build.sh
+++ example/build.sh
@@ -21,5 +21,6 @@ for pkg in 'corelib@1.0.0' 'corelib@1.2.3' 'corelib@1.6.0' 'extralib@1.0.0' ; do
     [ -f "$DIST/$pkg.phar" ] && rm -f "$DIST/$pkg.phar" || true
     find -name '*~' -delete
     PHAR pack -f "$DIST/$pkg.phar" -s "../empty-stub.php" .
+    php $MAIN/scripts/concat-php.php $( find -name '*.php' | grep -v pathload.php ) >"$DIST/$pkg.php"
   popd
 done
